dc2c1b1203b6f32d8c048a0d3efcea140c5abf10
miya9022 <miya9022@gmail.com>
2022-05-08 18:21:30 +0700

    Update new test ERC20 Token

--- test/token.ts
+++ test/token.ts
@@ -4,11 +4,13 @@ import {
   deployDiamond,
   FacetCutAction,
   getSelectors,
+  // eslint-disable-next-line node/no-missing-import
 } from "../scripts/deploy/diamond";
+// eslint-disable-next-line node/no-missing-import
 import { BasketFacet } from "../typechain";
-import { Contract, Signer } from "ethers";
+import { BigNumber, Contract, Signer } from "ethers";
 import { expect } from "chai";
-import { parseEther } from "ethers/lib/utils";
+import { parseEther, parseUnits } from "ethers/lib/utils";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
 describe("Token transaction", () => {
@@ -74,7 +76,148 @@ describe("Token transaction", () => {
     await basketFacet.connect(signer).addToken(mockToken.address);
   });
 
-  it("Should transfer token", async () => {
-    const receipient = accounts[1];
+  it("Should get token info", async () => {
+    const name = await mockToken.name();
+    expect(name).to.be.eq("MockERC20");
+
+    const symbol = await mockToken.symbol();
+    expect(symbol).to.be.eq("Mock");
+  });
+
+  describe("ERC20 Transfer", () => {
+    it("Should transfer from owner to accounts[1]", async () => {
+      const recipient = await accounts[1].getAddress();
+      await mockToken.connect(signer).transfer(recipient, parseEther("1000"));
+
+      const balance = await mockToken.balanceOf(recipient);
+      expect(balance).to.be.eq(parseEther("1000"));
+    });
+
+    it("Should transfer from accounts[1] to accounts[2] exceeds balance", async () => {
+      const amountTransfer = parseEther("1001");
+      const recipient2 = await accounts[2].getAddress();
+
+      try {
+        await mockToken
+          .connect(accounts[1])
+          .transfer(recipient2, amountTransfer);
+      } catch (e) {
+        // eslint-disable-next-line no-unused-expressions
+        expect(e).to.not.be.null;
+      }
+
+      const balance = await mockToken.balanceOf(recipient2);
+      expect(balance).to.be.eq("0");
+    });
+
+    it("Should transfer from accounts[1] to accounts[2]", async () => {
+      const amountTransfer = parseEther("999.99999999999");
+      const recipient2 = await accounts[2].getAddress();
+
+      await mockToken.connect(accounts[1]).transfer(recipient2, amountTransfer);
+
+      const balance = await mockToken.balanceOf(recipient2);
+      expect(balance).to.be.eq(amountTransfer);
+    });
+  });
+
+  describe("ERC20 TransferFrom", () => {
+    let spender: string;
+
+    before(async () => {
+      spender = await signer.getAddress();
+      await mockToken
+        .connect(signer)
+        .transfer(await accounts[1].getAddress(), parseEther("10000000"));
+
+      const receiver = await accounts[2].getAddress();
+      const balance: BigNumber = await mockToken.balanceOf(receiver);
+      await mockToken.connect(signer).burn(balance, receiver);
+    });
+
+    it("Should transfer from accounts[1] to accounts[2] without approval of accounts[1] for signer to spend its account", async () => {
+      const fromAddress = await accounts[1].getAddress();
+      const toAddress = await accounts[2].getAddress();
+
+      try {
+        await mockToken
+          .connect(signer)
+          .transferFrom(fromAddress, toAddress, parseEther("100"));
+      } catch (e) {
+        // eslint-disable-next-line no-unused-expressions
+        expect(e).to.not.be.null;
+      }
+    });
+
+    it("Should apply new allowance amount from accounts[1] to signer", async () => {
+      await mockToken.connect(accounts[1]).approve(spender, parseEther("100"));
+      const owner = await accounts[1].getAddress();
+      // get allowance
+      const allowance = await mockToken.allowance(owner, spender);
+      expect(allowance).to.be.eq(parseEther("100"));
+    });
+
+    it("Should transfer from accounts[1] to accounts[2]", async () => {
+      const fromAddress = await accounts[1].getAddress();
+      const toAddress = await accounts[2].getAddress();
+
+      await mockToken
+        .connect(signer)
+        .transferFrom(fromAddress, toAddress, parseEther("100"));
+      const balance = await mockToken.balanceOf(toAddress);
+      expect(balance).to.be.eq(parseEther("100"));
+    });
+  });
+
+  describe("ERC20 Mint Token", () => {
+    let testAccount: SignerWithAddress;
+    let totalSupply: BigNumber;
+
+    before(async () => {
+      testAccount = accounts[3];
+      totalSupply = await mockToken.totalSupply();
+      console.log(totalSupply.toString());
+    });
+
+    it("Should mint token to accounts[3]", async () => {
+      const address = await testAccount.getAddress();
+      await mockToken.mint(parseUnits("10", "kwei"), address);
+
+      const balance = await mockToken.balanceOf(address);
+      expect(balance).to.be.eq(parseUnits("10", "kwei"));
+      const newTotalSupply = await mockToken.totalSupply();
+      expect(newTotalSupply).to.be.eq(
+        totalSupply.add(parseUnits("10", "kwei"))
+      );
+      console.log(newTotalSupply.toString());
+    });
+  });
+
+  describe("ERC20 Burn Token", () => {
+    let testAccount: SignerWithAddress;
+    let totalSupply: BigNumber;
+
+    before(async () => {
+      testAccount = accounts[4];
+      totalSupply = await mockToken.totalSupply();
+      console.log(totalSupply.toString());
+
+      await mockToken
+        .connect(signer)
+        .transfer(await testAccount.getAddress(), parseUnits("11", "kwei"));
+    });
+
+    it("Should burn token in accounts[4]", async () => {
+      const address = await testAccount.getAddress();
+      await mockToken.burn(parseUnits("10", "kwei"), address);
+
+      const balance = await mockToken.balanceOf(address);
+      expect(balance).to.be.eq(parseUnits("1", "kwei"));
+      const newTotalSupply = await mockToken.totalSupply();
+      expect(newTotalSupply).to.be.eq(
+        totalSupply.sub(parseUnits("10", "kwei"))
+      );
+      console.log(newTotalSupply.toString());
+    });
   });
 });
