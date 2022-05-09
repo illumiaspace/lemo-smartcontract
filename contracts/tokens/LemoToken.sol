a320a8c463498acf94ef93509f20c63a4f14cf65
miya9022 <miya9022@gmaill.com>
2022-05-09 18:30:08 +0700

    Update new contract

--- contracts/tokens/LemoToken.sol
+++ contracts/tokens/LemoToken.sol
@@ -2,9 +2,12 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract LemoToken is ERC20 {
+contract LemoToken is ERC20, Ownable {
     constructor()
     ERC20("Let's move", "LEMO")
-    {}
+    {
+        _mint(msg.sender, 1000000000 ether);
+    }
 }
--- contracts/tokens/PaceToken.sol
+++ contracts/tokens/PaceToken.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract PaceToken is ERC20 {
+contract PaceToken is ERC20, Ownable {
     constructor()
     ERC20("Pace", "PACE")
-    {}
+    {
+        _mint(msg.sender, 1000000000 ether);
+    }
 
     function mint(uint256 _amount, address _issuer) public onlyOwner {
         _mint(_issuer, _amount);
